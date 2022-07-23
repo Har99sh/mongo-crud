eabc56fe2916c30f7828c77aed2a8f44565deeb1
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-07-23 19:07:05 +0200

    created views for routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.send('login page');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.send('signup page');
+});
+
 module.exports = router;
